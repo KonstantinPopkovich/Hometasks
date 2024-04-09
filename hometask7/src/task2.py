3d229c7a71105244710bde5f96923cc8c8675eba
Konstantin Popkovich <kopopkovich0397@gmail.com>
2024-04-09 13:01:01 +0300

    Deletion

--- hometask7/src/task2.py
+++
@@ -1,8 +0,0 @@
-#Написать функцию которая принимает 2 слова и проверяет являются ли они анограммами друг друга.
-#True если да False если нет. Решить наиболее оптимальным путем.
-
-
-def anagrams(word1, word2):
-    return sorted(word1.lower()) == sorted(word2.lower())
-
-print(anagrams("Taxi", "Ixat"))
--- hometask7/src/task3.py
+++
@@ -1,8 +0,0 @@
-#Написать функцию которая определяет читается ли слово с двух концов одинаково
-# True если да False если нет. Решить наиболее оптимальным путем.
-
-
-def palindrome(word):
-    word = word.lower()
-    return word == word[::-1]
-print(palindrome("MOUSuom"))
