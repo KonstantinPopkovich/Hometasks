763e35c9555de30101b38918f82d2ec330a92513
Konstantin Popkovich <kopopkovich0397@gmail.com>
2024-04-09 08:21:29 +0300

    Hometask7

---
+++ hometask7/src/task4.py
@@ -0,0 +1,19 @@
+#Есть список слов аннограмм ["asd", "dsa", "tar", "rat", "bla"]
+# сгруппировать аннограммы к такому виду [["asd", "dsa], ["tar", "rat"], ["bla"]]
+# - допустимая сложность алгоритма O(n)
+def groups_of_anagrams(words):
+    groups = {}
+    for word in words:
+        #sorted_word = ''.join(sorted(word))
+        word_hash = 0
+        for char in word:
+            word_hash += ord(char)
+        if word_hash in groups:
+            groups[word_hash].append(word)
+        else:
+            groups[word_hash] = [word]
+
+    return list(groups.values())
+
+word_list = ["asd", "dsa", "tar", "rat", "bla"]
+print(groups_of_anagrams(word_list))
